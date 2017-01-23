1366b9c0dfcad6a26aa19a2545fa6b706cd76722
Oleg Viengo <viengooleg@gmail.com>
2017-01-23 22:52:07 +0200

    init app

---
+++ app/contructor/constructor.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Constructor = () => (
+  <div className="main">
+    <div className="wrap">
+      <header />
+      <section />
+    </div>
+    <footer />
+  </div>
+);
