f7e8f65e9c1e7a3a5363612d1d1edff24a065cda
Oleg Viengo <viengooleg@gmail.com>
2017-01-23 22:37:27 +0200

    'init'

--- app/index.js
+++ app/index.js
@@ -1,8 +1,9 @@
-
+import React from 'react';
+import ReactDom from 'react-dom';
 
 ReactDom.render(
-    <main>
-        <h2>Hello world!</h2>
-    </main>,
-    document.getElementById('root')
+  <main>
+    <h2>Hello world!</h2>
+  </main>,
+  document.getElementById('root'),
 );
